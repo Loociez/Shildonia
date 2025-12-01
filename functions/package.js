35d1dbb00638c1212f5cea9dee7b3d043736fcf7
Loociez <loocielambert@gmail.com>
2025-12-01 23:09:56 +0000

    RCON test

---
+++ functions/package.js
@@ -0,0 +1,13 @@
+{
+  "name": "rcon-api",
+  "version": "1.0.0",
+  "engines": { "node": "18" },
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5",
+    "firebase-admin": "^11.11.0",
+    "firebase-functions": "^4.4.0",
+    "rcon-client": "^5.0.0",
+    "express-rate-limit": "^6.7.0"
+  }
+}
